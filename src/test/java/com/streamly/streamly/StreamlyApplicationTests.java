43a0f51945c6b75a1d5e9bd1b75d7a1efc291b95
Recionds <56007790+Recionds@users.noreply.github.com>
2026-01-16 23:19:10 +0900

    setting : 초기 환경 설정

---
+++ src/test/java/com/streamly/streamly/StreamlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamly.streamly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
