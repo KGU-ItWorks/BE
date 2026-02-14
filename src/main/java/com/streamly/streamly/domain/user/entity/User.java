9bede3e540c11f89c70d10819634e5ef339a9a27
Recionds <56007790+Recionds@users.noreply.github.com>
2026-02-15 00:16:45 +0900

    debug : user 테이블명 변경 (#13)
    * deploy : 배포 설정(docker설정 및 workflows설정)
    
    * deploy : 배포 설정 수정(k8s 사용불가로 docker compose 전환)
    
    * debug : prod 설정 파일 수정
    
    * debug : prod 설정 파일 수정
    
    * debug : PostgreSQL user예약어 충돌로 user 테이블 -> users로 변경

--- src/main/java/com/streamly/streamly/domain/user/entity/User.java
+++ src/main/java/com/streamly/streamly/domain/user/entity/User.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity { // BaseEntity는 생성/수정일 자동 관리용
